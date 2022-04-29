0138ff5d26b5a5931411edef3c6cdf467b371bb0
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-04-29 15:56:54 +0200

    comentarios

--- EDAanalisis.py
+++ EDAanalisis.py
@@ -3,30 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("USA_Housing.csv")
-data.head()
-data.columns
+data = pd.read_csv("USA_Housing.csv")#Leemos el archivo csv
+data.head()#sacamos las primeras 5 filas de datos
+data.columns#Vemos cuáles son las columnas de nuestro archivo
 
-data.isnull().sum()
-data.duplicated().sum()
-data.info()
-data.describe()
+data.isnull().sum()#Usamos un método para ver si hay datos nulos y que devuelva la suma de todos los datos nulos que encuentre
+data.duplicated().sum()#Usamos un método para ver si hay datos duplicados y que devuelva la suma de todos los datos duplicados que encuentre
+data.info()#Vemos qué tipo de datos hay en el dataset
+data.describe()#Sacamos los datos estadísticos del dataset
 
-data.hist()
-data.hist("Area Population")
+data.hist()#Histograma del dataset
+data.hist("Area Population")#Histograma de la columna "Area Population"
 
-data.plot()
+data.plot()#Plot del dataset
+plt.show()#Enseñamos el plot
 
-plt.show()
+sns.heatmap(data.corr(), annot=True)#Mapa de calor del dataset
+plt.show()#Enseñamos el gráfico
 
-sns.heatmap(data.corr(), annot=True)
-plt.show()
+sns.pairplot(data)#Pairplot del dataset
+plt.show()#Enseñamos el pairplot
 
-sns.pairplot(data)
-plt.show()
+sns.boxplot(data=data, x='Avg. Area Income', y='Area Population')#Diagrama de cajas
+plt.show()#Enseñamos el diagrama
 
-#sns.boxplot(data=data, x='Avg. Area Income', y='Area Population')
-
-for column in data:
+for column in data:#Bucle que enseña los histplots de cada columna
         sns.histplot(x=data[column])
         plt.show()
---
+++ train.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
